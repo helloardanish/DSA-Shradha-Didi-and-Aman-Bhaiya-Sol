2cc2fbb51d014de4a36191a4ae9d669935974edd
A R DANISH <helloardanish@gmail.com>
2022-05-09 00:43:49 +0530

    Create 002_Reverse_array.cpp

---
+++ C++/Arrays/002_Reverse_array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> reverseArray(vector<int> arr){
+  int start=0;
+  int end=arr.size()-1;
+  while(start < end){
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    ++start;
+    --end;
+  }
+  return arr;
+}
+
+int main()
+{
+    vector<int> myarr1 {2,3,5,4,100,6,2,3,3};
+    //vector<int> myarr1 {2,3,4,1,4,5};
+    
+
+    vector<int> ans = reverseArray(myarr1);
+    
+    for(auto x:ans){
+      cout << x << " ";
+    }
+    cout << endl;
+  
+    return 0;
+}
