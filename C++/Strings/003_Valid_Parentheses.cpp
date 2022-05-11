6f297edbc05ad0983d4a25319e25520f0f198362
A R DANISH <helloardanish@gmail.com>
2022-05-11 21:38:28 +0530

    Update 003_Valid_Parentheses.cpp

--- C++/Strings/003_Valid_Parentheses.cpp
+++ C++/Strings/003_Valid_Parentheses.cpp
@@ -32,3 +32,35 @@ public:
 };
 
 
+// I used extra space for storing set of dictionary using unordered map.
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> charsetmap = {
+          {'(' , ')'},
+          {'{' , '}'},
+          {'[' , ']'},
+        };
+
+        stack<char> charst;
+        
+        for(char ch:s){
+            if(!(charsetmap.find(ch) == charsetmap.end())){
+                charst.push(ch);
+            }else{
+                if(charst.empty() || charsetmap[charst.top()]!=ch){
+                    return false;
+                }
+                charst.pop();
+            }
+        }
+        return charst.empty();
+    }
+};
+
+
+// Though this solution isn't optimal as the first one but when we'll have a large of set of characters we can prefer this code. We don't have code out like 10 condition.
+// Hope you understand it :)
+
